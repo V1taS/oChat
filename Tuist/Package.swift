66e6c7af0f88ca2982e948f40d06e8f0625c8d90
V1taS <SosinVitalii@iCloud.com>
2024-06-05 18:47:16 +0200

    Improve chat

--- Tuist/Package.swift
+++ Tuist/Package.swift
@@ -11,16 +11,16 @@ import PackageDescription
 let package = Package(
   name: "Package",
   dependencies: [
-   .package(url: "https://github.com/V1taS/MessengerSDK", exact: "1.3.0"),
+   .package(url: "https://github.com/V1taS/MessengerSDK", exact: "1.4.0"),
    .package(url: "https://github.com/V1taS/AuthenticationSDK", exact: "1.3.0"),
    .package(url: "https://github.com/V1taS/SKStoriesWidget", exact: "1.0.0"),
    .package(url: "https://github.com/V1taS/SKUIKit", exact: "1.2.0"),
    .package(url: "https://github.com/V1taS/Wormholy", exact: "1.0.0"),
-   .package(url: "https://github.com/V1taS/SKServices", exact: "1.8.0")
+   .package(url: "https://github.com/V1taS/SKServices", exact: "1.9.0")
     
     // .package(path: "../../../../General/SKAbstractions"),
-    // .package(path: "../../../../General/MessengerSDK"),
-    // .package(path: "../../../../General/SKServices"),
+//     .package(path: "../../../../General/MessengerSDK"),
+//     .package(path: "../../../../General/SKServices"),
     // .package(path: "../../../../General/SKUIKit")
   ]
 )
