09fe45eda10575e4976ebf358e80f66a36710381
Sosin Vitalii <30578049+V1taS@users.noreply.github.com>
2024-06-30 23:33:15 +0200

    Fix optional text (#18)

--- LocalDependencies/MessengerSDK/Sources/MessengerSDK/Modules/MessengerDialogScreenModule/Factory/MessengerDialogScreenFactory.swift
+++ LocalDependencies/MessengerSDK/Sources/MessengerSDK/Modules/MessengerDialogScreenModule/Factory/MessengerDialogScreenFactory.swift
@@ -106,7 +106,7 @@ extension MessengerDialogScreenFactory: MessengerDialogScreenFactoryInput {
     // Создаем объект User
     let user = User(
       id: contactModel.id,
-      name: contactModel.name ?? contactModel.toxAddress?.formatString(minTextLength: 10),
+      name: (contactModel.name ?? contactModel.toxAddress?.formatString(minTextLength: 10)) ?? "",
       avatarURL: nil,
       isCurrentUser: false
     )
